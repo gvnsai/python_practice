fcc6eeaea7dce691992e85e8cf52c761ee16a091
gvnsai <42485121+gvnsai@users.noreply.github.com>
2018-08-21 00:04:23 +0530

    Create assignment1.py

---
+++ assignment1.py
@@ -0,0 +1,14 @@
+# Check input data is string or int or int in the string.
+
+try:
+    input_data=eval(input('Enter your input:'))
+    if type(input_data) == int:
+        print(input_data,' is int type')  
+    elif type(input_data) == str:
+        if input_data.isdigit():
+            print(input_data,' is int in string')
+        else:
+            print(input_data,' is sting only')
+except NameError:
+    print('is string')
+
