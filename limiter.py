1135e15f37a9dac24332daefca7807ba554ce204
gvnsai <42485121+gvnsai@users.noreply.github.com>
2018-10-09 12:34:22 +0530

    Add files via upload

---
+++ limiter.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+
+app=Flask(__name__)
+limiter = Limiter(app, key_func=get_remote_address)
+
+@app.route('/api')
+@limiter.limit("5 per day")
+def api():
+	return 'hello'
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
